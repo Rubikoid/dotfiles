bf654a862cb0201355e59f5fb0f529edb49508fe
Rubikoid <rubikoid@rubikoid.ru>
2021-02-01 02:46:43 +0300

    latex

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/latex_indentconfig.yaml ~/.indentconfig.yaml
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 mkdir -p ~/.config
