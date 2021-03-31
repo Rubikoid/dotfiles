f8ebd5bea52c227bd464243c1f3c66ff47322c84
Rubikoid <rubikoid@rubikoid.ru>
2021-03-31 23:01:55 +0300

    Add ssh secret config

--- init.sh
+++ init.sh
@@ -1,6 +1,18 @@
+# tmux
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
+# latex
 ln -s ~/dotfiles/latex_indentconfig.yaml ~/.indentconfig.yaml
+
+# zshrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
+
+# vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
+
+# cheat
 mkdir -p ~/.config
 ln -s ~/dotfiles/cheat ~/.config/cheat
+
+# ssh
+ln -s ~/dotfiles/ssh/config ~/.ssh/config
